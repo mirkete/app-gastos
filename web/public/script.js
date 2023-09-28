452d85a2935b07ddf37dc2a779011fdabba6fa33
Mirko Saenz Valiente <saenzvalientemirko@gmail.com>
2023-09-27 23:52:40 -0300

    dark-light switch implemented

--- web/public/script.js
+++ web/public/script.js
@@ -1,5 +1,22 @@
-function crearHtmlGrupo ({ name }) {
-  return `<article class="grupo">
+const themeSwitch = document.querySelector('.theme-switch')
+const html = document.querySelector('html')
+themeSwitch.addEventListener('click', (e) => {
+  e.stopPropagation()
+  e.preventDefault()
+  if (themeSwitch.classList.contains('switch-on')) {
+    themeSwitch.classList.add('switch-off')
+    html.classList.remove('dark')
+    return themeSwitch.classList.remove('switch-on')
+  }
+  themeSwitch.classList.add('switch-on')
+  html.classList.add('dark')
+  themeSwitch.classList.remove('switch-off')
+})
+
+const colores = ['green', 'yellow', 'lightblue', 'orange', 'red']
+
+function crearHtmlGrupo ({ name, i }) {
+  return `<article style="background-color: ${colores[i]};" class="grupo">
     <div class="izquierda-grupo parte-grupo">
       <span class="big-text">${name}</span>
     </div>
@@ -9,13 +26,12 @@ function crearHtmlGrupo ({ name }) {
     </div>
   </article>`
 }
-
 async function obtenerTeams () {
   const teamsRequest = await fetch('http://192.168.1.3:3000/interactions/user-teams/90914877-0d9d-4881-bdce-87e7dc406bf2')
   const teams = await teamsRequest.json()
   // En el model, pedir el balance de ese grupo
-  const html = teams.map((team) => {
-    return crearHtmlGrupo({ name: team.name })
+  const html = teams.map((team, i) => {
+    return crearHtmlGrupo({ name: `Group ${i + 1}`, i })
   }).join('')
   document.getElementById('groups').innerHTML += html
 }
